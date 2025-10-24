1ef9003c7e96ae7fa770596f07a8ec901d310242
Niraj Paudel <itsmenirajpaudel@gmail.com>
2025-10-24 14:37:51 +1300

    This is a test commit

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,2 @@
 export const SLACK_USER_EMAIL = "nirazlatu@gmail.com";
-export const FIRST_COMMIT_RECOGNITION: string = `🎉 *Well done!*\nYou’ve just made your *first commit* — that’s where every great project begins. Keep the momentum going, one commit at a time. 🚀`;
+export const FIRST_COMMIT_RECOGNITION: string = `🎉 *Well done Niraj!*\nYou’ve just made your *first commit* — that’s where every great project begins. Keep the momentum going, one commit at a time. 🚀`;
